9d8651f55654b1273215ba826402b89cec03c2f3
Frederico Peres Correia <fredericoperescorreia@hotmail.com>
2025-11-15 11:15:35 +0000

    obter pois ao longo de uma rota

--- src/main/java/com/myapp/model/Route.java
+++ src/main/java/com/myapp/model/Route.java
@@ -3,18 +3,23 @@ package com.myapp.model;
 import java.util.List;
 
 public class Route {
+    private final List<Point> routePoints;
     private final double distanceKm;
     private final long durationSec;
     private final TransportMode mode;
     private final List<POI> pois;
 
-    public Route(double distanceKm, long durationSec, TransportMode mode, List<POI> pois) {
+    public Route(List<Point> routePoints, double distanceKm, long durationSec, TransportMode mode, List<POI> pois) {
+        this.routePoints = routePoints;
         this.distanceKm = distanceKm;
         this.durationSec = durationSec;
         this.mode = mode;
         this.pois = pois;
     }
 
+    public List<Point> getRoutePoints() {
+        return routePoints;
+    }
     public double getDistanceKm() {
         return distanceKm;
     }
